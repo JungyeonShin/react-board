5daf90edf4df690d9f149be273d78b3dcb79ef67
JungyeonShin <feelsagood@naver.com>
2024-06-27 14:23:18 +0900

    proxy관련 첫번쨰 시도

--- src/api/client.js
+++ src/api/client.js
@@ -5,7 +5,9 @@ const URL = `${PROXY}`;
 
 const client = axios.create({
     //baseURL: 'http://localhost:3000',
-    baseURL: import.meta.env.VITE_BACKEND_AXIOS_URL + PROXY,
+    baseURL: window.location.hostname === 'localhost'
+                ? import.meta.env.VITE_BACKEND_AXIOS_URL + PROXY
+                : URL,
     timeout: 10000,
     xsrfCookieName: 'XSRF-TOKEN',
     xsrfHeaderName: 'X-XSRF-TOKEN',
